fc5bf68b399ddf1c3f5f14099f38a0ebab30f1e3
SHARGINJAY PATEL <sharginjaypatel12021999@gmail.com>
2022-12-11 18:11:33 +0530

    OKK

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void  main(String[] args)
+  {
+      RWOnly r= new RWOnly();
+      //String ans = r.name ;
+      r.setName("Jay") ;
+      System.out.println(r.getName());
+  }
 }
